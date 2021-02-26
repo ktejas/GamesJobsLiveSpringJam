b5e039e7c81126f54708a3105f46d217bc97c6ca
ktejas <krishnasalimeda@gmail.com>
2021-02-26 20:01:18 +0530

    Changed camera perspective

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -6,10 +6,13 @@ public class CameraManager : MonoBehaviour
 {
     private float rotationSpeed = 10.0f;
 
+    private Vector3 startPosition;
+
     void Start()
     {
         // Camera rotates around center
-        transform.LookAt(new Vector3(8.26f, 3f, 3.37f));
+        //transform.LookAt(new Vector3(8.26f, 3f, 3.37f));
+        startPosition = transform.position;
     }
 
     void Update()
@@ -17,11 +20,11 @@ public class CameraManager : MonoBehaviour
         // Rotating camera around a point (first parameter), around the Y axis (second parameter) at 20 degrees per second * speed
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.RotateAround(new Vector3(8.26f, 3f, 3.37f), new Vector3(0.0f, 2.0f, 0.0f), 10 * Time.deltaTime * rotationSpeed);
+            transform.RotateAround(startPosition, new Vector3(0.0f, 2.0f, 0.0f), 10 * Time.deltaTime * rotationSpeed);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.RotateAround(new Vector3(8.26f, 3f, 3.37f), new Vector3(0.0f, 2.0f, 0.0f), 10 * Time.deltaTime * -rotationSpeed);
+            transform.RotateAround(startPosition, new Vector3(0.0f, 2.0f, 0.0f), 10 * Time.deltaTime * -rotationSpeed);
         }
 
         // Increasing/decreasing height of camera
